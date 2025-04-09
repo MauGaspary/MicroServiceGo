4ebece150c5557d9a288ecb5b8ee3b6db5aedb42
Maurício <maugaspary@gmail.com>
2025-04-09 14:19:16 -0300

    Refatorei o código criando as pastas application e handler

--- main.go
+++ main.go
@@ -2,29 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"context"
+	"github.com/MauGaspary/MicroServiceGo/application"
 )
 
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Erro ao escutar o servidor", err)
+		fmt.Println("Falha ao iniciar o app:", err)
 	}
-}
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Ane eu te amo!"))
 }
